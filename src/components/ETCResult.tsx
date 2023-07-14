fcef9eab2529e255575ee2756d92666e59da8f20
ohwoo-kwon <kwonow1@gmail.com>
2023-07-14 14:45:16 +0900

    etc 완료

--- src/components/ETCResult.tsx
+++ src/components/ETCResult.tsx
@@ -5,6 +5,9 @@ const ContentDiv = styled.div`
   height: 100%;
   background-color: #fff;
   border-radius: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
   padding: 30px 50px;
   box-sizing: border-box;
   overflow: scroll;
@@ -15,8 +18,49 @@ const ContentDiv = styled.div`
   }
 `;
 
+const ContentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: space-between;
+  margin-bottom: 30px;
+`;
+
+const Title = styled.div`
+  width: 100%;
+  margin-bottom: 10px;
+  font-size: 18px;
+`;
+
+const ContentStrength = styled.div`
+  text-align: center;
+  font-weight: 600;
+  font-size: 24px;
+  width: 85%;
+  color: #8b93f7;
+`;
+
+const ContentPosition = styled.div`
+  text-align: center;
+  font-weight: 600;
+  font-size: 24px;
+  width: 85%;
+  color: #fa6986;
+`;
+
 const ETCResult = () => {
-  return <ContentDiv>ETCResult</ContentDiv>;
+  return (
+    <ContentDiv>
+      <ContentBox>
+        <Title>적합도</Title>
+        <ContentStrength>72%</ContentStrength>
+      </ContentBox>
+      <ContentBox>
+        <Title>포지션 추천</Title>
+        <ContentPosition>프론트엔드 개발</ContentPosition>
+      </ContentBox>
+    </ContentDiv>
+  );
 };
 
 export default ETCResult;
